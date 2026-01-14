69d14dd941bd295bd5a16c630fde0842eb888483
unknown <edu6194294@agderskolen.no>
2026-01-14 11:09:58 +0100

    La til docker

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './Components/Navbar'
 import './App.css'
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
   return (
     <>
     <h1>Heisann ass</h1>
+    <h1>{count}</h1>
     <h1>Jeg heter Kristoffer</h1>
+
+    <Navbar />
     </>
   )
 }
