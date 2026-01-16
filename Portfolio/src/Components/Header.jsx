cd16d526aaa33c45bf983991f2d25ee3c728b039
unknown <Kristofferihmelesteberg@gmail.com>
2026-01-16 13:41:46 +0100

    Endret bakgrunn og lagt til en font

--- Portfolio/src/Components/Header.jsx
+++ Portfolio/src/Components/Header.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Navbar from './Navbar'
 import { Link } from 'react-router-dom'
 
+import styles from '../Components/Header.module.css'
+
 const Header = () => {
   return (
     <>
-      <header>
+      <header className={styles.header}>
         <div className='name'>
           <Link to={"/"}>Kristoffer</Link>
         </div>
--- Portfolio/src/Components/Navbar.jsx
+++ Portfolio/src/Components/Navbar.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom'
 import Projects from '../Pages/Projects'
 import About from '../Pages/About'
 
+import styles from '../Components/Navbar.module.css'
+
 const Navbar = () => {
   return (
     <>
-      <div className='navbar'>
+      <div className={styles.navbar}>
         <Link to={"/prosjekter"}>Projects</Link>
         <Link to={"/kristoffer"}>About</Link>
         <Link to={"/ferdigheter"}>Teknologier</Link>
