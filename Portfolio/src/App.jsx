1dcbf87f3480256dd44f22a130f5cc977a7f95df
Hanne Ihme <Kristofferihmelesteberg@gmail.com>
2026-01-18 23:18:24 +0100

    La til footer

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -6,13 +6,18 @@ import './App.css'
 import './assets/CSS/global.css'
 import { Link } from 'react-router-dom'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
 
   return (
     <>
       <Header />
-      <h1>Kristoffer</h1>
+      <div className=''>
+        <h1>Heisann</h1>
+      </div>
+
+      <Footer />
     </>
   )
 }
---
+++ Portfolio/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import styles from '../Components/Footer.module.css'
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <h1 className={styles.footerHeader}>heisann</h1>
+    </div>
+  )
+}
+
+export default Footer
