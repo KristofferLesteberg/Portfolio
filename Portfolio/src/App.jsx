181ec6bbd74bc179893ebb8f9286facf466afc32
unknown <Kristofferihmelesteberg@gmail.com>
2026-01-20 11:31:23 +0100

    Lagt til projects.json: her skal prosjektene legges inn. Begynt å implementere globale css variabler også

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './Components/Navbar'
-import './App.css'
-import './assets/CSS/global.css'
+
 import { Link } from 'react-router-dom'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 
+import "./assets/CSS/global.css"
+
 import 'animate.css'
 
 function App() {
-
   return (
     <>
       <Header />
+      
       <div className='frontPage animate__animated animate__fadeInUp'>
         <h1 className='frontHeading'>
           Heisann! <br />
@@ -28,14 +27,10 @@ function App() {
               Denne porteføljen viser utvalgte prosjekter og ferdigheter jeg har utviklet gjennom praktisk læring og kontinuerlig utforsking av teknologi.
            </h2>
       </div>
-
         <Link to={"/prosjekter"}>
           <button className='frontBtn'>Se hva jeg har lagd!</button>
         </Link>
       </div>
-      
-    
-
       <Footer />
     </>
   )
--- Portfolio/src/Components/Header.jsx
+++ Portfolio/src/Components/Header.jsx
@@ -14,7 +14,6 @@ const Header = () => {
         <div className='navbar'>
           <Navbar />
         </div>
-        
       </header>
     </>
     
