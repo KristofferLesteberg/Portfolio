015967c9067c3c08169732217f096bbcd87389c3
unknown <Kristofferihmelesteberg@gmail.com>
2026-01-21 12:20:29 +0100

    Lagt til bilde på forside

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -8,49 +8,51 @@ import Footer from './Components/Footer'
 import "./assets/CSS/global.css"
 
 import 'animate.css'
+import picture from './assets/Pictures/CV_Bilde.png'
 
 function App() {
   return (
     <>
       <Header />
       <div className='frontPage animate__animated animate__fadeInUp'>
-        <h1 className='frontHeading'>
-          Heisann! <br />
-          Jeg er Kristoffer Lesteberg
-        </h1>
+        <div className='front'>
+          <h1 className='frontHeading'>
+            Heisann! <br />
+            Jeg er Kristoffer Lesteberg
+          </h1>
+        <img className='frontImg' src={picture} alt="" />
+        </div>
+       
       <div className='frontText'>
             <h2>
-              Jeg er en 17 år gammel teknologiinteressert utvikler med fokus på programmering og digitale løsninger. <br />
-              Jeg har erfaring med webutvikling og programvareutvikling gjennom egne prosjekter, og jobber strukturert <br />
+              Jeg er en 17 år gammel teknologiinteressert utvikler med fokus på <br />
+              programmering og digitale løsninger.
+              Jeg har erfaring med webutvikling<br />
+               og programvareutvikling gjennom egne prosjekter, og jobber strukturert <br />
               med både frontend og logikk. <br />
-              Denne porteføljen viser utvalgte prosjekter og ferdigheter jeg har utviklet gjennom praktisk læring og kontinuerlig utforsking av teknologi.
+              Denne porteføljen viser utvalgte prosjekter og ferdigheter jeg har utviklet gjennom <br />
+              praktisk læring og kontinuerlig utforsking av teknologi.
            </h2>
       </div>
         <Link to={"/prosjekter"}>
           <button className='frontBtn'>Se hva jeg har lagd!</button>
         </Link>
       </div>
-    <main>
+    <main className='frontMain'>
       <div className='Me'>
         <h1 className='meHeading'>Hvem er jeg?</h1>
           <div className='school'>
             <h1>Elev ved Sam Eyde Vgs.</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed aperiam omnis mollitia labore eum, sequi ab similique iste dicta quaerat obcaecati porro sapiente at voluptate qui animi blanditiis, alias culpa.</p>
-
+              <p>Jeg er elev på Sam Eyde vgs. Jeg går i andre klasse på linjen informasjonsteknologi.</p>
           </div>
           <div className='hobbies'>
 
           </div>
-          <div className='instrests'></div>
+          <div className='instrests'>
 
-
+          </div>
       </div>
     </main>  
-
-
-      <br />
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
       <Footer />
     </>
   )
