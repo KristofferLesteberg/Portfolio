0bfc1d2358f540ca7028384117b4427e6cc9a794
unknown <Kristofferihmelesteberg@gmail.com>
2026-01-27 20:15:58 +0100

    Lagt til bilder, hovereffect og skygger. Lagt til lorem tekst for placeholder.

--- Portfolio/src/Components/AboutMe.jsx
+++ Portfolio/src/Components/AboutMe.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 import styles from './AboutMe.module.css'
+import SamEyde from '../assets/Pictures/SamEyde.jpg'
+import codeExample from '../assets/Pictures/codeExampleImg.jpg'
+import leader from '../assets/Pictures/LeaderExampleimg.jpg'
 
 const AboutMe = () => {
   return (
@@ -9,14 +12,19 @@ const AboutMe = () => {
             <h1 className={styles.aboutMeh1}>Hvem er Jeg?</h1>
             <div className={styles.container}>
                 <div className={`${styles.school} ${styles.item}`}>
-                <h1>Elev ved Sam Eyde vgs.</h1>
-               
+                <img src={SamEyde} />
+                <h2>Elev ved Sam Eyde vgs.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam provident illo blanditiis pariatur veniam similique perspiciatis eligendi quibusdam eum id et, placeat maiores excepturi vitae, quaerat consequatur eveniet praesentium neque.</p>
             </div>
             <div className={`${styles.hobbies} ${styles.item}`}>
-                <h1>Daglig leder</h1>
+                <img src={leader} />
+                <h2>Daglig leder</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quaerat odio quos magni. Excepturi quia ut magni libero corrupti. Blanditiis ullam error vel sint deleniti. Aliquam quidem saepe corrupti molestias!</p>
             </div>
             <div className={`${styles.intrest} ${styles.item}`}>
-                <h1>Teknologi intressert</h1>
+                <img src={codeExample} />
+                <h2>Teknologi intressert</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nihil, pariatur error at officia dolorum esse beatae dolore, tempora laborum tenetur eum fugit! Architecto, veritatis reprehenderit laudantium dolorem tempora quibusdam.</p>
 
             </div>
 
